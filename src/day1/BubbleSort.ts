9f8252d76bc34b1907037718582e6c80be288d26
ismi-abbas <muhdabbas98@gmail.com>
2023-02-14 00:37:08 +0800

    Day 1

--- src/day1/BubbleSort.ts
+++ src/day1/BubbleSort.ts
@@ -1,3 +1,23 @@
-export default function bubble_sort(arr: number[]): void {
-
+export default function bubble_sort(arr: number[]): number[] {
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr.length - 1 - i; j++) {
+            // always minus one of the length because everytime we execute, the last index will always be the largest
+            // and we shorten the array now
+            // then we swap the position of the largest index
+            if (arr[j] > arr[j + 1]) {
+                // put the temp value as arr[j]
+                const tmp = arr[j];
+                // swap it by assign arr[j] to arr[j+1]
+                arr[j] = arr[j + 1];
+                // assign arr[j+1] to temp
+                arr[j + 1] = tmp;
+            }
+        }
+    }
+    return arr;
 }
+
+console.log({
+    1: bubble_sort([3, 4, 7, 9, 1, 42, 69, 420]),
+    2: bubble_sort([10, 9, 8, 7, 6, 5, 4, 3]),
+});
--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,22 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
-
+    const jumpAmount = Math.floor(Math.sqrt(breaks.length));
+    let i = jumpAmount;
+    for (; i < breaks.length; i += jumpAmount) {
+        if (breaks[i]) {
+            break;
+        }
+    }
+    i -= jumpAmount;
+    for (let j = 0; j < jumpAmount && i < breaks.length; ++j, ++i) {
+        if (breaks[i]) {
+            return i;
+        }
+    }
+    return -1;
 }
+
+// [f,f, .... t, ...]
+// 0          ^     N
+
+// linear walk and find t it will be O(n) - linear
+// we will jump by square root of N and running time will be square root of N
