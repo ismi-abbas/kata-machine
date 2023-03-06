f8941353e53cae6d55de750a79058bb3ff6ccdd0
abbas <abbaspuzi.dev@gmail.com>
2023-03-06 23:02:53 +0800

    LinkedList - Queue

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,22 +1,22 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
-    let low = 0;
-    let high = haystack.length;
-    do {
-        const midpoint = Math.floor(low + (high - low) / 2);
-        const value = haystack[midpoint];
-        if (value === needle) {
-            return true;
-        } else if (value > needle) {
-            high = midpoint;
-        } else {
-            low = midpoint + 1;
-        }
-    } while (low < high);
-    return false;
+  let low = 0
+  let high = haystack.length
+  do {
+    const midpoint = Math.floor(low + (high - low) / 2)
+    const value = haystack[midpoint]
+    if (value === needle) {
+      return true
+    } else if (value > needle) {
+      high = midpoint
+    } else {
+      low = midpoint + 1
+    }
+  } while (low < high)
+  return false
 }
 
-let result = bs_list([1, 3, 4, 69, 71, 81, 90, 99, 420, 1337, 69420], 69);
-console.log(result);
+let result = bs_list([1, 3, 4, 69, 71, 81, 90, 99, 420, 1337, 69420], 69)
+console.log(result)
 
 /* 
 - search(array, hi, lo)
--- src/day1/SinglyLinkedList.ts
+++ src/day1/SinglyLinkedList.ts
@@ -1,27 +1,36 @@
 export default class SinglyLinkedList<T> {
     public length: number;
 
-    
+
 
     constructor() {
     }
 
     prepend(item: T): void {
-
-}
+        return undefined
+    }
     insertAt(item: T, idx: number): void {
-
-}
+        return undefined
+    }
     append(item: T): void {
-
-}
+        return undefined
+    }
     remove(item: T): T | undefined {
+        return undefined
 
-}
+    }
     get(idx: number): T | undefined {
-
-}
+        return undefined
+    }
     removeAt(idx: number): T | undefined {
+        return undefined
+    }
+}
 
-}
-}
+// Time and complexity ===========
+// prepend: O(1)
+// insertAt: O(n)
+// deletion from end: O(n)
+// getHead: O(1)
+// getTail: O(n)
+// get: O(n)
